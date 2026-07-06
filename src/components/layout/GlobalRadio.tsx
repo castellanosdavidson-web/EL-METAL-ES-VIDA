1a0eb0e03e171a9997f31f63bb16219868cfa86e
Davidson <davidson@elmetalesvida.com>
2026-07-06 16:08:01 -0500

    style: Clean up radio station names in selector

--- src/components/layout/GlobalRadio.tsx
+++ src/components/layout/GlobalRadio.tsx
@@ -2,9 +2,9 @@
 import React, { useState, useRef, useEffect } from 'react';
 
 const STATIONS = [
-  { id: 'rockantenne', name: 'Rock Antenne Heavy (General)', url: 'https://stream.rockantenne.de/heavy-metal/stream/mp3', tagline: 'Clásicos y Heavy Metal 24/7' },
-  { id: 'maximusrock', name: 'Maximus Rock FM (Español)', url: 'https://stream.laut.fm/maximusrockfm', tagline: 'Rock & Metal en Castellano' },
-  { id: 'distorsion', name: 'Distorsión FM (Chile)', url: 'https://tunein.radiomaniacos.cl/distorsion.mp3', tagline: 'Metal Latinoamericano y Clásico' },
+  { id: 'rockantenne', name: 'Rock Antenne Heavy', url: 'https://stream.rockantenne.de/heavy-metal/stream/mp3', tagline: 'Clásicos y Heavy Metal 24/7' },
+  { id: 'maximusrock', name: 'Maximus Rock FM', url: 'https://stream.laut.fm/maximusrockfm', tagline: 'Rock & Metal en Castellano' },
+  { id: 'distorsion', name: 'Distorsión FM', url: 'https://tunein.radiomaniacos.cl/distorsion.mp3', tagline: 'Metal Latinoamericano y Clásico' },
   { id: 'nevermore', name: 'Nevermore Radio', url: 'https://panel.nevermorenetwork.com/listen/nevermorenetwork.com/radio.mp3', tagline: 'Programación Extrema y Entrevistas' }
 ];
 
