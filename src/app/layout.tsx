a201c17562d3571122791aa1c5e15743701e1709
Davidson <davidson@elmetalesvida.com>
2026-07-07 12:57:26 -0500

    feat: install Google Tag Manager container GTM-TWWHZ5P3 in Root Layout head and body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,6 +83,16 @@ export default function RootLayout({
       className={`${syne.variable} ${jetbrains.variable} ${hanken.variable} dark notranslate`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TWWHZ5P3');`
+          }}
+        />
         <meta name="google" content="notranslate" />
         <link rel="icon" href={logoUrl} />
         <link rel="apple-touch-icon" href={logoUrl} />
@@ -92,6 +102,15 @@ export default function RootLayout({
         />
       </head>
       <body className="bg-background text-on-background antialiased selection:bg-primary-container selection:text-on-primary-container">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TWWHZ5P3"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <LayoutWrapper>
           {children}
         </LayoutWrapper>
