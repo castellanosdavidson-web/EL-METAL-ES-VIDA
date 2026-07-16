5bf88708e14c8d558075d2710fbdc551ede8a8c1
Davidson <davidson@elmetalesvida.com>
2026-07-16 17:20:38 -0500

    Config: Inject Meta (Facebook) Pixel for analytics and remarketing

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -133,6 +133,12 @@ export default async function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
+        {/* Meta (Facebook) Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1363200675758067'); fbq('track', 'PageView');`
+          }}
+        />
       </head>
       <body className="bg-background text-on-background antialiased selection:bg-primary-container selection:text-on-primary-container">
         {/* Google Tag Manager (noscript) */}
@@ -143,6 +149,10 @@ export default async function RootLayout({
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
           />
+        </noscript>
+        {/* Meta (Facebook) Pixel (noscript) */}
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=1363200675758067&ev=PageView&noscript=1" alt="" />
         </noscript>
         <NextIntlClientProvider messages={messages}>
           <LayoutWrapper>
