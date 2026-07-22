ca185da0a7f25e6514dfd7db62187fa441237440
Davidson <davidson@elmetalesvida.com>
2026-07-22 14:09:05 -0500

    Config: Allow all external image domains to fix broken CD covers

--- next.config.ts
+++ next.config.ts
@@ -8,33 +8,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'dxmaslijicgzrwfmzkuv.supabase.co',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
       {
-        protocol: 'https',
-        hostname: 'pub-51ef8e7084d14c629323a477cb4a596f.r2.dev',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media-amazon.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
+        protocol: 'http',
+        hostname: '**',
       },
     ],
   },
