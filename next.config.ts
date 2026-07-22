d7631911e2fcb92618b28e33469648f62f93f7a4
Davidson <davidson@elmetalesvida.com>
2026-07-22 14:30:15 -0500

    Config: Disable Next.js image optimization globally to fix Vercel 402 Image Optimization limits

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
